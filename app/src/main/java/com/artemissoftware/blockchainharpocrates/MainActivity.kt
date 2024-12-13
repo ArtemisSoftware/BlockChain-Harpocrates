cdfcc623f75249fdf53fa3f0925e06b2e5df41f5
gustavo <gustavo.isaac@gmail.com>
2024-12-13 12:25:55 +0000

    BlockChainRepository

--- app/src/main/java/com/artemissoftware/blockchainharpocrates/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/blockchainharpocrates/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.artemissoftware.blockchainharpocrates.datastore.source.DataStoreSource
+import com.artemissoftware.blockchainharpocrates.repository.BlockChainRepository
 import com.artemissoftware.blockchainharpocrates.ui.theme.BlockchainHarpocratesTheme
 
 class MainActivity : ComponentActivity() {
---
+++ app/src/main/java/com/artemissoftware/blockchainharpocrates/repository/BlockChainRepository.kt
@@ -0,0 +1,94 @@
+package com.artemissoftware.blockchainharpocrates.repository
+
+import com.artemissoftware.blockchainharpocrates.datastore.source.DataStoreSource
+import com.artemissoftware.blockchainharpocrates.models.Block
+import com.artemissoftware.blockchainharpocrates.util.HashUtil
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+
+class BlockChainRepository (
+    private val dataSource: DataStoreSource
+) {
+
+    private val blocks: MutableList<Block> = mutableListOf()
+    private var difficulty = 0;
+    private val _blockChainFlow = MutableStateFlow<List<Block>>(emptyList())
+    val blockChainFlow: StateFlow<List<Block>> get() = _blockChainFlow.asStateFlow()
+
+
+    suspend fun startChain(){
+        dataSource.getAppSettings().collectLatest {
+            difficulty = it.proofOfWork
+            if(blocks.isEmpty()) {
+                val block = Block(data = "Genesis Block")
+                block.mineBlock(difficulty)
+                blocks.add(block)
+                _blockChainFlow.value = blocks.toList()
+            }
+        }
+    }
+
+    fun updateBlockChain(message: String) {
+        addBlock(message)
+        if(isBlockChainValid) {
+            _blockChainFlow.value = blocks.toList()
+        }
+    }
+
+
+    suspend fun updateDifficulty(pow: Int) {
+        dataSource.setProofOfWork(pow)
+    }
+
+    private fun addBlock(data: String) = with(blocks.last()) {
+        val block = Block(
+            index = index + 1,
+            previousHash = hash,
+            data = data
+        )
+        block.mineBlock(difficulty)
+        blocks.add(block)
+    }
+
+    private val isBlockChainValid: Boolean
+        get() {
+            if (!isFirstBlockValid) {
+                return false
+            }
+            for (i in 1 until blocks.size) {
+                val currentBlock = blocks[i]
+                val previousBlock = blocks[i - 1]
+                if (!isValidNewBlock(currentBlock, previousBlock)) return false
+            }
+            return true
+        }
+
+    private val isFirstBlockValid: Boolean
+        get() {
+            val firstBlock = blocks[0]
+            return when {
+                (firstBlock.index != 0) -> {
+                    return false
+                }
+                (firstBlock.previousHash != null) -> {
+                    return false
+                }
+                else -> firstBlock.hash != null && HashUtil.calculate(text = firstBlock.str()) == firstBlock.hash
+            }
+        }
+
+    private fun isValidNewBlock(newBlock: Block, previousBlock: Block): Boolean {
+        return when{
+            (previousBlock.index + 1 != newBlock.index) -> {
+                false
+            }
+            (newBlock.previousHash == null) -> {
+                false
+            }
+            else -> newBlock.hash != null && HashUtil.calculate(text = newBlock.str()) == newBlock.hash
+        }
+    }
+
+}
